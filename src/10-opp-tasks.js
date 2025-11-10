6dc33a9d5f40af8cf3dfb2f4c957fbcc345ac3fc
kiqiou <kuksare@gmail.com>
2025-11-10 14:53:15 +0300

    oop

--- src/10-opp-tasks.js
+++ src/10-opp-tasks.js
@@ -1,57 +1,28 @@
-/**
- * You will complete the PaginationHelper class,
- * which is a utility class helpful for querying paging information related to an array.
- * The class is designed to take in an array of values and an integer
- * indicating how many items will be allowed per each page.
- * The types of values contained within the collection/array are not relevant.
- *
- * The following are some examples of how this class is used:
- * @example
- *     const helper = new PaginationHelper(['a','b','c','d','e','f'], 4);
- *     helper.pageCount(); //should == 2
- *     helper.itemCount(); //should == 6
- *     helper.pageItemCount(0); //should == 4
- *     helper.pageItemCount(1); // last page - should == 2
- *     helper.pageItemCount(2); // should == -1 since the page is invalid
- *
- *     // pageIndex takes an item index and returns the page that it belongs on
- *     helper.pageIndex(5); //should == 1 (zero based index)
- *     helper.pageIndex(2); //should == 0
- *     helper.pageIndex(20); //should == -1
- *     helper.pageIndex(-10); //should == -1
- */
-
 class PaginationHelper {
-  // The constructor takes in an array of items and a integer indicating how many
-  // items fit within a single page
-  constructor(/* collection, itemsPerPage */) {
-    throw new Error('Not implemented');
+  constructor(collection, itemsPerPage) {
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage;
   }
 
-  // returns the number of items within the entire collection
   itemCount() {
-    console.log(this);
-    throw new Error('Not implemented');
+    return this.collection.length;
   }
 
-  // returns the number of pages
   pageCount() {
-    console.log(this);
-    throw new Error('Not implemented');
+    return Math.ceil(this.collection.length / this.itemsPerPage);
   }
 
-  // returns the number of items on the current page. page_index is zero based.
-  // this method should return -1 for pageIndex values that are out of range
-  pageItemCount() {
-    console.log(this);
-    throw new Error('Not implemented');
+  pageItemCount(pageIndex) {
+    if (pageIndex < 0 || pageIndex >= this.pageCount()) return -1;
+    if (pageIndex === this.pageCount() - 1) {
+      return this.collection.length % this.itemsPerPage || this.itemsPerPage;
+    }
+    return this.itemsPerPage;
   }
 
-  // determines what page an item is on. Zero based indexes
-  // this method should return -1 for itemIndex values that are out of range
-  pageIndex() {
-    console.log(this);
-    throw new Error('Not implemented');
+  pageIndex(itemIndex) {
+    if (itemIndex < 0 || itemIndex >= this.collection.length) return -1;
+    return Math.floor(itemIndex / this.itemsPerPage);
   }
 }
 
